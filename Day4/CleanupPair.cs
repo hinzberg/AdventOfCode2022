6790bafb9921965c49b95c345985805f61a42176
Holger Hinzberg <holger@hinzberg.de>
2022-12-04 14:42:47 +0100

    Day 4: Camp Cleanup

---
+++ Day4/CleanupPair.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace Day4
+{
+	public class CleanupPair
+	{
+		private string _input;
+		private string _firstPart;
+		private string _secondPart;
+		private Tuple<int, int> _firstRange;
+		private Tuple<int, int> _secondRange;
+		public bool DoesCompleteIntersect = false;
+		public bool DoesPartialIntersect = false;
+
+		public CleanupPair(string input)
+		{
+			var parts = input.Split(",");
+			_input = input;
+			_firstPart = parts[0];
+			_secondPart = parts[1];
+			_firstRange = GetRange(_firstPart);
+			_secondRange = GetRange(_secondPart);
+            DoesCompleteIntersect = CheckCompleteIntersection(_firstRange, _secondRange);
+            DoesPartialIntersect = CheckPartialIntersection(_firstRange, _secondRange);
+		}
+
+		private Tuple<int, int> GetRange(string input)
+		{
+			var parts = input.Split("-");
+			string startString = parts[0];
+			string endString = parts[1];
+			int start = int.Parse(startString);
+			int end = int.Parse(endString);
+			return new Tuple<int, int>(start, end);
+		}
+
+		private bool CheckCompleteIntersection(Tuple<int, int> range1, Tuple<int, int> range2)
+		{
+			// Range 1 contains Range 2
+			if (range2.Item1 >= range1.Item1 && range2.Item2 <= range1.Item2)
+				return true;
+
+			// Range 2 contains Range 1
+			if (range1.Item1 >= range2.Item1 && range1.Item2 <= range2.Item2)
+				return true;
+
+			return false;
+		}
+
+		private bool CheckPartialIntersection(Tuple<int, int> range1, Tuple<int, int> range2)
+		{
+			return range1.Item1 <= range2.Item2 && range2.Item1 <= range1.Item2;
+		}
+	}
+}
+
---
+++ Day4/Program.cs
@@ -0,0 +1,29 @@
+﻿using Day4;
+
+Console.WriteLine("Day 4: Camp Cleanup");
+
+string path = @"input.txt";
+
+List<CleanupPair> pairs = new List<CleanupPair>();
+
+using (StreamReader sr = new StreamReader(path))
+{
+    while (sr.Peek() != -1)
+    {
+        var line = sr.ReadLine();
+        if (!string.IsNullOrEmpty(line))
+        {
+            var p = new CleanupPair(line);
+            pairs.Add(p);
+        }
+    }
+}
+
+var completeIntersectCount = pairs.Count(a => a.DoesCompleteIntersect);
+Console.WriteLine($"Complete Intersect Count: {completeIntersectCount}");
+
+var partialntersectCount = pairs.Count(a => a.DoesPartialIntersect);
+Console.WriteLine($"Partial Intersect Count: {partialntersectCount}");
+
+Console.WriteLine("Done");
+
