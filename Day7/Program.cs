98feffcf303fca0a093bb129dc4009422c02eaa8
Holger Hinzberg <holger@hinzberg.de>
2022-12-11 11:17:41 +0100

    Added Helper Class FileReader

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -1,52 +1,17 @@
 ﻿using Day7;
+using HelperClasses;
 
 Console.WriteLine("Day 7: No Space Left On Device");
 
-/*
-string input = "$ cd /\n";
-input += "$ ls\n";
-input += "dir a\n";
-input += "14848514 b.txt\n";
-input += "8504156 c.dat\n";
-input += "dir d\n";
-input += "$ cd a\n";
-input += "$ ls\n";
-input += "dir e\n";
-input += "29116 f\n";
-input += "2557 g\n";
-input += "62596 h.lst\n";
-input += "$ cd e\n";
-input += "$ ls\n";
-input += "584 i\n";
-input += "$ cd ..\n";
-input += "$ cd ..\n";
-input += "$ cd d\n";
-input += "$ ls\n";
-input += "4060174 j\n";
-input += "8033020 d.log\n";
-input += "5626152 d.ext\n";
-input += "7214296 k\n";
-*/
-
 string path = @"input.txt";
-string content = string.Empty;
 
 AdvDirectoryCrawler crawler = new AdvDirectoryCrawler();
 
-using (StreamReader sr = new StreamReader(path))
-{
-    while (sr.Peek() != -1)
-    {
-        var line = sr.ReadLine();
-        if (!string.IsNullOrEmpty(line))
-        {
-            crawler.ParseCommand(line);
-        }
-    }
-}
+List<string> lines = FileReader.Read(path);
+foreach (var line in lines)
+    crawler.ParseCommand(line);
 
 int currentSpaceUsed = crawler.GetTotalSizeOfRootDirectory();
 crawler.FindSmallestDirectoryToDelete(currentSpaceUsed);
-
 
 Console.WriteLine("Done");
---
+++ HelperClasses/FileReader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HelperClasses
+{
+	public static class FileReader
+	{
+		public static List<string> Read(string fileName)
+		{
+            List<string> lines = new List<string>();
+
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                while (sr.Peek() != -1)
+                {
+                    var line = sr.ReadLine();
+                    if (!string.IsNullOrEmpty(line))
+                    {
+                        lines.Add(line);
+                    }
+                }
+            }
+            return lines;
+        }
+	}
+}
+
